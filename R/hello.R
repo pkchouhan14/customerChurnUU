db0777940229c45c44cea3a940cb8961e0405430
Pushpinder <p.chouhan@ulster.ac.uk>
2018-10-26 13:18:21 +0100

    First commit

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' @title Customer Churn data analysis
+#'
+#' @param x
+#'
+#' @return confusion matrix of the model
+#'
+#' @export
+#'
+#' @examples
+#' hello("Akul")
+#' \dontrun{
+#' hello("Me")
+#' }
+hello <- function(x) {
+  print(paste0("Hello ", x, ", world!"))
+}
